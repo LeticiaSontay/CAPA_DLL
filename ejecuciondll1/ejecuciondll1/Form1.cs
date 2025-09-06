3682c640289d14998dd1051c84ce32870834d20c
Leticia Sontay <lsontayc1@miumg.edu.gt>
2025-09-05 23:55:40 -0600

    se sube capa dll

---
+++ ejecuciondll1/ejecuciondll1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejecuciondll1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cargardatos();
+        }
+        void cargardatos()
+        {
+            combo1.llenarse("empleados", "codigo_empleado", "puesto");
+
+        }
+    }
+}
